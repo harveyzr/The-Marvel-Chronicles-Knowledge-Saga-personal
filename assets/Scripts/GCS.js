cb1944a02ce5feaed6eefd73445c32e0871f7ced
Masonmanshark4 <149852525+Masonmanshark4@users.noreply.github.com>
2024-03-22 10:59:32 -0400

    modal adjustments

---
+++ assets/Scripts/GCS.js
@@ -0,0 +1,7 @@
+const btnback = document.getElementById('btn-back')
+const modal = document.getElementById('modal')
+const actpbtn = document.getElementById('actpbtn')
+
+btnback.addEventListener('click', () => {
+    modal.style.display = 'block';
+});
