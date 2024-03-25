21f03b4d253a920368bb1c6afa4367686cca355a
DakotaPatterson <158085432+DakotaPatterson@users.noreply.github.com>
2024-03-25 18:57:03 -0400

    Fixed it

--- assets/Scripts/MarvelQ.js
+++ assets/Scripts/MarvelQ.js
@@ -216,12 +216,12 @@ function clearHighScores() {
     
     var quizContainer = document.getElementById('quiz-container');
     if (quizContainer) {
-        document.getElementById('quiz-container').innerHTML += highScoresHtml; 
+        quizContainer.innerHTML = ''; // Clear the quiz container's content
     }
 
 }
 
-document.getElementById("restartQuiz").addEventListener("click", window.location.reload());
+document.getElementById("restartQuiz").addEventListener("click", restartQuiz);
 document.getElementById("clearHighScores").addEventListener("click", clearHighScores);
 
 // Load a random character when the page loads
